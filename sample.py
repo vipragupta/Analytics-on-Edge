0af9a933e989afb0d4919c7e9401b621af5025d4
Chaitra Ramachandra <chaitrapatgar@gmail.com>
2017-04-22 21:55:24 -0600

    Added nested user menu and json data to send in requests

--- sample.py
+++ sample.py
@@ -1,10 +1,83 @@
 import webbrowser
 import sys
 import os
+import requests
+from flask import Flask
+import json
+
+
+graphMenuDict = {1:"distance", 2:"elevation", 3:"calories", 4:"pulse", 5:"floors", 6:"bp", 7:"steps"}
+durationMenuDict = {1:"daily", 2:"weekly", 3:"monthly", 4:"yearly"}
+
+#----------------------------------userMenu-------------------------------------
+def userMenu(clientId):
+    try:
+        mainMenuChoice = 0
+        while(mainMenuChoice != 3):
+            mainMenu = "======USER MENU======\n1. View Today's summary\n2. Generate Graphs\n3. Close the Application\n"
+            print mainMenu
+            print "Enter your choice: "
+            mainMenuChoice = int(input())
+
+            if(mainMenuChoice == 1):
+                createJsonData(mainMenuChoice, 0, clientId)
+                #displayTodaySummary(mainMenuChoice, clientId)
+
+            elif(mainMenuChoice == 2):
+                graphMenu = "\n======GRAPH MENU======\n1. Distance\n2. Elevation\n3. Calories\n4. Pulse\n5. Floors\n6. Blood Pressure\n7. Steps\n"
+                print graphMenu
+                print "Which graph do you want to generate?: "
+                graphMenuChoice = int(input())
+
+                durationMenu = "\n======DURATION MENU======\n1. Daily\n2. Weekly\n3. Monthly\n4. Yearly\n"
+                print durationMenu
+                print "Please select the time-frame for graph: "
+                durationMenuChoice = int(input())
+
+                if((graphMenuChoice >= 1 and graphMenuChoice <=7) and (durationMenuChoice >=1 and durationMenuChoice <= 4)):
+                    createJsonData(graphMenuChoice, durationMenuChoice, clientId)
+                    #displayGraph(graphMenuChoice, durationMenuChoice, clientId)
+                else:
+                    print "Wrong Choice!"
+
+            elif(mainMenuChoice == 3):
+                print "Goodbye!"
+
+            else:
+                print "Wrong Choice!"
+
+    except:
+		comment = ('EXCEPTION: ' + str(sys.exc_info()[1]))
+		print comment
+		return 0
+
+#--------------------------------createJSONData---------------------------------
+def createJsonData(graphMenuChoice, durationMenuChoice, clientId):
+    if(durationMenuChoice == 0):
+        data = {"clientId":clientId}
+    else:
+        data = {"clientId":clientId, "type":graphMenuDict[graphMenuChoice], "duration":durationMenuDict[durationMenuChoice]}
+    data = json.dumps(data)
+    print data
+
+    #url = 'http://ES_search_demo.com/document/record/_search?pretty=true'
+    #response = requests.post(url, data=data)
+
+
+#--------------------------------performOperation-------------------------------
+def displayTodaySummary(mainMenuChoice, clientId):
+    print mainMenuChoice
+
+
+#--------------------------------performOperation-------------------------------
+#@app.route('/some-url')
+def displayGraph(graphMenuChoice, durationMenuChoice, clientId):
+    print "Your graphMenuChoice: " + str(graphMenuChoice)
+    print "Your durationMenuChoice: " + str(durationMenuChoice)
 
 
 #---------------------------------HTMLReport------------------------------------
-def HTMLReport():
+def htmlReport():
 	try:
 		reportName = "heartbeat"
 		clientID = "100100"
@@ -14,7 +87,7 @@ def HTMLReport():
 		htmlReportPath = os.path.dirname(os.path.realpath(__file__))
 		htmlReportPath = os.path.join(htmlReportPath,"report.html")
 		print htmlReportPath
-		htmlReport = open(htmlReportPath,"w")
+		htmlReportFp = open(htmlReportPath,"w")
 
 		# write html document
 		global html
@@ -95,16 +168,16 @@ def HTMLReport():
 		<br/>
 		"""
 
-		htmlReport.write(html)
+		htmlReportFp.write(html)
 
 		# write all closing tags
-		htmlReport.write('</div>')
-		htmlReport.write('</body>')
-		htmlReport.write('</html>')
+		htmlReportFp.write('</div>')
+		htmlReportFp.write('</body>')
+		htmlReportFp.write('</html>')
 
 		# print results to shell
 		print "Created html report"
-		htmlReport.close()
+		htmlReportFp.close()
 
 		webbrowser.get().open(htmlReportPath)
 
@@ -116,4 +189,7 @@ def HTMLReport():
 		return 0
 
 if __name__ == '__main__':
-    HTMLReport()
+    clientId = raw_input("Please enter your ID: ")
+    print clientId
+    userMenu(clientId)
+    #htmlReport()
