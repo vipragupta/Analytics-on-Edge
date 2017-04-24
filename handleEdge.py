2893fb33beb00abd89570076f66cd42a563fd63c
nachiket bhagwat <nachiketbhagwat@gmail.com>
2017-04-24 03:25:04 -0600

    seperated to handlers

--- handleEdge.py
+++ handleEdge.py
@@ -42,50 +42,5 @@ def pushdata():#from edge server
         localSummary(inter, db)
     return jsonify({'StatusCode':'200','Message': 'Database addition/replacement success'})
 
-@app.route('/getreport', methods=['GET', 'POST'])
-def getreport():#from client
-    req = request.json
-    print str(req), type(req), "\n"
- 
-    if "duration" not in req:
-        print "duration not in req"
-        return jsonify({'StatusCode':'400','Message': 'Invalid request, please provide duration, clientid, time and other details'})
-    
-    duration = req["duration"]
-    print "duration = " + str(duration) + "\n"
-    if(duration == "dailyall"):
-        ret = dailyAll(req, db)
-        ret['StatusCode'] = 200
-        ret['Message'] = 'Summary data retrieved'
-        return json.dumps(ret)
-    elif(duration == "daily"):
-        ret = daily(req, db)
-        ret['StatusCode'] = 200
-        ret['Message'] = 'Hourwise data retrieved'
-        return json.dumps(ret)
-    elif(duration == "weekly"):
-        ret = weekly(req, db)
-        print ret
-        ret['StatusCode'] = 200
-        ret['Message'] = 'Daywise data retrieved'
-        return json.dumps(ret)
-    elif(duration == "yearly"):
-        ret = yearly(req, db)
-        print ret
-        ret['StatusCode'] = 200
-        ret['Message'] = 'Monthwise data retrieved'
-        return json.dumps(ret)
-    elif(duration == "localAreaSummary"):
-        ret = localAreaSummary(req, db)
-        print ret
-        ret['StatusCode'] = 200
-        ret['Message'] = 'Local summary data retrieved'
-        return json.dumps(ret)
-    '''
-    elif(duration == "monthly"):
-        #do something
-    '''
-    return jsonify({'StatusCode':'200','Message': 'Database search result'})
-
 #if __name__ == '__main__':
 #    app.run(debug=True)
