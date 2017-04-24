6f85824912ad3c86e22d47181453a084fd6cc49e
vipragupta <guptavipra@gmail.com>
2017-04-23 18:23:17 -0600

    edge api done

--- edge_server.py
+++ edge_server.py
@@ -3,6 +3,14 @@ import time
 import random
 from datetime import datetime
 from random import randint
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+import json
+import ast
+
+app = Flask(__name__)
+api = Api (app)
+
 
 class Edge():
 
@@ -23,10 +31,12 @@ class Edge():
 
 
 	def printDic(self):
+		print
 		for id in self.master:
 			print id, ": ", self.master[id]
 		print
 		print "CLIENT SUMMARY: ",self.clientSummary
+		print
 		print "LOCAL SUMMARY: ",self.localSummary
 
 
@@ -36,113 +46,128 @@ class Edge():
 
 
 	def __processClientSummary(self, newData):
+		data = ast.literal_eval(newData["data"])
 		clientSum = self.clientSummary[newData["clientId"]] if newData["clientId"] in self.clientSummary else {}
-		map = self.__processData(clientSum, newData["data"])
-		
-		clientSum["steps"]     = map["steps"]
-		clientSum["distance"]  = map["distance"]
-		clientSum["elevation"] = map["elevation"]
-		clientSum["calories"]  = map["calories"]
-		clientSum["floors"]    = map["floors"]
-		clientSum["active"]    = map["active"]
-		clientSum["pulse"]     = map["pulse"]
-		clientSum["bp"]        = map["bp"]
-		clientSum["num"]       = map["num"]
-		self.clientSummary[newData["clientId"]] = clientSum
+		map = self.__processData(clientSum, data)
+		if len(map) > 0:
+			clientSum["steps"]     = map["steps"]
+			clientSum["distance"]  = map["distance"]
+			clientSum["elevation"] = map["elevation"]
+			clientSum["calories"]  = map["calories"]
+			clientSum["floors"]    = map["floors"]
+			clientSum["active"]    = map["active"]
+			clientSum["pulse"]     = map["pulse"]
+			clientSum["bp"]        = map["bp"]
+			clientSum["num"]       = map["num"]
+			self.clientSummary[newData["clientId"]] = clientSum
+
 
 	def __processLocalSummary(self, newData):
-		map = self.__processData(self.localSummary, newData["data"])
-		num = int(map["num"])
+		data = ast.literal_eval(newData["data"])
+		map = self.__processData(self.localSummary, data)
+		if len(map) > 0:
+			num = int(map["num"])
 
-		self.localSummary["steps"]     = str(int(map["steps"])       / 2)
-		self.localSummary["distance"]  = str(float(map["distance"])  / 2)
-		self.localSummary["elevation"] = str(float(map["elevation"]) / 2)
-		self.localSummary["calories"]  = str(float(map["calories"])  / 2)
-		self.localSummary["floors"]    = str(float(map["floors"])    / 2)
-		self.localSummary["active"]    = str(float(map["active"])    / 2)
-		self.localSummary["pulse"]     = map["pulse"]
-		self.localSummary["bp"]        = map["bp"]
-		self.localSummary["num"]       = map["num"]
+			self.localSummary["steps"]     = str(int(map["steps"])       / 2)
+			self.localSummary["distance"]  = str(float(map["distance"])  / 2)
+			self.localSummary["elevation"] = str(float(map["elevation"]) / 2)
+			self.localSummary["calories"]  = str(float(map["calories"])  / 2)
+			self.localSummary["floors"]    = str(float(map["floors"])    / 2)
+			self.localSummary["active"]    = str(float(map["active"])    / 2)
+			self.localSummary["pulse"]     = map["pulse"]
+			self.localSummary["bp"]        = map["bp"]
+			self.localSummary["num"]       = map["num"]
 
 	def __processData(self, oldDataMap, newData):
-		map = {}
+		try:
+			map = {}
+			num = (1 if (len(oldDataMap) == 0) else int(oldDataMap["num"])+1)
+			
+			if int(newData['steps']) > 0:
+				newData["active"] = "30"
+			else:
+				newData["active"] = "0"
 
-		num = (1 if (len(oldDataMap) == 0) else int(oldDataMap["num"])+1)
-
-		if int(newData['steps']) > 0:
-			newData["active"] = "30"
-		else:
-			newData["active"] = "0"
-
-		map["steps"]     = ("0" if (len(oldDataMap) == 0) else str(int(oldDataMap["steps"]) + int(newData["steps"])))
-		map["distance"]  = ("0" if (len(oldDataMap) == 0) else str(float(oldDataMap["distance"])  + float(newData["distance"])))
-		map["elevation"] = ("0" if (len(oldDataMap) == 0) else str(float(oldDataMap["elevation"]) + float(newData["elevation"])))
-		map["calories"]  = ("0" if (len(oldDataMap) == 0) else str(float(oldDataMap["calories"])  + float(newData["calories"])))
-		map["floors"]    = ("0" if (len(oldDataMap) == 0) else str(float(oldDataMap["floors"])    + float(newData["floors"])))
-		
-		map["active"]= ("0" if (len(oldDataMap) == 0) else str(float(oldDataMap["active"]) + float(newData["active"])))
-
-		map["pulse"] = self.__getAveragePulse((0 if (len(oldDataMap) == 0) else int(oldDataMap["pulse"])) + int(newData["pulse"]), 2)
-		map["bp"]    = self.__getBp(("0" if (len(oldDataMap) == 0) else oldDataMap["bp"]), newData["bp"], 2)
-		map["num"] = str(num)
-		return map
+			map["steps"]     = ("0" if (len(oldDataMap) == 0) else str(int(oldDataMap["steps"]) + int(newData["steps"])))
+			map["distance"]  = ("0" if (len(oldDataMap) == 0) else str(float(oldDataMap["distance"])  + float(newData["distance"])))
+			map["elevation"] = ("0" if (len(oldDataMap) == 0) else str(float(oldDataMap["elevation"]) + float(newData["elevation"])))
+			map["calories"]  = ("0" if (len(oldDataMap) == 0) else str(float(oldDataMap["calories"])  + float(newData["calories"])))
+			map["floors"]    = ("0" if (len(oldDataMap) == 0) else str(float(oldDataMap["floors"])    + float(newData["floors"])))
+			
+			map["active"]= ("0" if (len(oldDataMap) == 0) else str(float(oldDataMap["active"]) + float(newData["active"])))
+			map["pulse"] = self.__getAveragePulse((0 if (len(oldDataMap) == 0) else int(oldDataMap["pulse"])) + int(newData["pulse"]), 2)
+			map["bp"]    = self.__getBp(("0" if (len(oldDataMap) == 0) else oldDataMap["bp"]), newData["bp"], 2)
+			map["num"] = str(num)
+			return map
+		except Exception as e:
+			print "EXCEPTION", str(e)
+			return {}
 
 
 
 	# Add data when client's that hour data is present also.
 	def addDataToExisitngHour(self, dataMap, datetm):
-		map = self.__processData(self.master[dataMap["clientId"]][self.getDate(datetm)][str(datetm.hour)], dataMap["data"])
-		self.master[dataMap["clientId"]][self.getDate(datetm)][str(datetm.hour)] = map
+		data = ast.literal_eval(dataMap["data"])
+		map = self.__processData(self.master[dataMap["clientId"]][self.getDate(datetm)][str(datetm.hour)], data)
+		if len(map) > 0:
+			self.master[dataMap["clientId"]][self.getDate(datetm)][str(datetm.hour)] = map
 
 
 
 	# Add data when client Data is present on edge for the given date, but it doesn't contain that hour data
 	def addDataToExisitingDate(self, dataMap, datetm):
-		clientData = self.__processData({}, dataMap["data"])
-		clientData["num"] = "1"
-		map1 = {str(datetm.hour): clientData}
-		self.master[dataMap["clientId"]][self.getDate(datetm)] = map1
+		data = ast.literal_eval(dataMap["data"])
+		clientData = self.__processData({}, data)
+		if len(clientData) > 0:
+			clientData["num"] = "1"
+			map1 = {str(datetm.hour): clientData}
+			self.master[dataMap["clientId"]][self.getDate(datetm)] = map1
 
 
 
 	# Add data when client Data is present on edge for the given date, but it doesn't contain that hour data
 	def addDataToExisitingClient(self, dataMap, datetm):
-		clientData = self.__processData({}, dataMap["data"])
-		clientData["num"] = "1"
+		data = ast.literal_eval(dataMap["data"])
+		clientData = self.__processData({}, data)
+		if len(clientData) > 0:
+			clientData["num"] = "1"
 
-		map1 = {str(datetm.hour): clientData}
-		map2 = {self.getDate(datetm): map1}
-		self.master[dataMap["clientId"]] = map2
+			map1 = {str(datetm.hour): clientData}
+			map2 = {self.getDate(datetm): map1}
+			self.master[dataMap["clientId"]] = map2
 
 
 	# Add data when client Data is not present also.
 	def addData(self, dataMap, datetm):
-		clientData = self.__processData({}, dataMap["data"])
-		clientData["num"] = "1"
-		map1 = {str(datetm.hour): clientData}
-		map2 = {self.getDate(datetm): map1}
-		self.master[dataMap["clientId"]] = map2
+		data = ast.literal_eval(dataMap["data"])
+		clientData = self.__processData({}, data)
+		if len(clientData) > 0:
+			clientData["num"] = "1"
+			map1 = {str(datetm.hour): clientData}
+			map2 = {self.getDate(datetm): map1}
+			self.master[dataMap["clientId"]] = map2
 
 
 
 	def __incrementCounters(self, dataMap):
-		print "New Data: ", dataMap
 		datetm = self.__convertStrToDate(dataMap["time"])
 		date = self.getDate(datetm)
 		hour = str(datetm.hour)
 
 		if (dataMap["clientId"] in self.master) and (date in self.master[dataMap["clientId"]]) and (hour in self.master[dataMap["clientId"]][date]):
+			#print "************"			
 			self.addDataToExisitngHour(dataMap, datetm)
 		
 		elif (dataMap["clientId"] in self.master) and (date in self.master[dataMap["clientId"]][date]) :
+			#print "#####################"
 			self.addDataToExisitingDate(dataMap, datetm)
 		
 		elif dataMap["clientId"] in self.master:
-			print (date in self.master[dataMap["clientId"]])
-			print
+			#print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 			self.addDataToExisitingClient(dataMap, datetm)
 
 		else:
+			#print "$$$$$$$$$$$$$$$$$$$$$$$$$"
 			self.addData(dataMap, datetm)
 
 		return "Done"
@@ -171,12 +196,13 @@ class Edge():
 		return str(sys) + "/" + str(dys)
 
 	def processIt(self, dataMap):
+		#data = ast.literal_eval(dataMap)
 		self.__incrementCounters(dataMap)
 		self.__processClientSummary(dataMap)
 		self.__processLocalSummary(dataMap)
 
 
-	def sendToCloud(self):
+	def getServerData(self):
 		finalList = {}
 		
 		client = {}
@@ -204,10 +230,10 @@ class Edge():
 				client[clientId] = clientMap
 
 		finalList["hourly"] = client
-		print finalList
+		#print finalList
 		return finalList
 
-
+edge = Edge()
 
 def extractData():
 	map = {}
@@ -223,49 +249,63 @@ def extractData():
 	return map 			#TODO
 
 
-def sendDataToCloud():
-	return True
+def checkSendDataToCloud():
 	now = datetime.now()
 	datetm = datetime.strptime(str(now), "%Y-%m-%d %H:%M:%S.%f")
-	
 	if datetm.minute == 00 or datetm.minute == 30:		#Reset all data at 00:05
 		return True
 	else:
 		return False
 
 
+def serverInteraction(map):
+	response = ""
+	i = 0
+	while i < 5:
+		if len(map) != 0:
+			jsonData = json.dumps(map)
+			print "JSON DATA: ",jsonData
+
+			url = 'http://127.0.0.1:5000/pushdata'
+			response = requests.post(url, data=jsonData, headers={"Content-Type":"application/json"})
+			response = response.json()
+
+			print "Response: ", response
+			if response['StatusCode'] == '200':
+				break
+			i += 1 
+			print "Server status not 200"
+		else:
+			break;
+
+
+@app.route('/pushSecondsData', methods=['GET', 'POST'])
+def mypost():
+	try:
+		if(request.method == 'POST'):
+			print "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+			print
+			print
+			print "Request: " , request.json
+			#json1_data = json.loads()
+			edge.processIt(request.json)
+			edge.printDic()
+			response = {'StatusCode':'200','Message':'Success'}
+			response = json.dumps(response)
+
+			if checkSendDataToCloud():
+				dic = edge.getServerData()
+			edge.checkResetData()
+			return response
+	
+	except Exception as e:
+		print "Error", str(e)
+		response = {'StatusCode':'404','Message':'Fail'}
+		response = json.dumps(response)
+		return jsonify({'error': str(e)})
+
+
 
 
 if __name__ == '__main__':
-
-    edge = Edge()
-    i = 0
-    #try:
-    while i < 50:
-        dataMap = extractData()
-        sendMessage = edge.processIt(dataMap) 								# where all the operations happen
-        
-        if sendMessage == None:
-            sendMessage = ""
-
-        #print("sendMessage: ", sendMessage)
-        edge.printDic()
-
-        
-        #edge.checkResetData()
-        #send = sock.sendto(sendMessage, client)
-        
-        print
-        print
-        i +=1
-
-    if sendDataToCloud():
-        edge.sendToCloud()
-    #except error, msg:
-    #    print ("Error during sending message.")
-    #    print ("ERROR CODE: ", msg[0])
-    #    print ("ERROR MESSAGE: ", msg[1])
-
-    #finally:
-    #    print ("closing socket")
-        #sock.close()
+	app.run(debug=True)
